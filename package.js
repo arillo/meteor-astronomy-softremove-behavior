785c152d8ae20fe92396718cbb717c80e6b0aa07
DM <dm@arillo.net>
2015-05-26 12:25:51 +0200

    updated to jagi:astronomy-behaviors@0.5.2 bump version to 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Softremove behavior for Meteor Astronomy',
-  version: '0.0.2',
+  version: '0.0.3',
   name: 'arillo:astronomy-softremove-behavior',
   git: 'https://github.com/arillo/meteor-astronomy-softremove-behavior.git'
 });
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy-behaviors@0.4.0');
+  api.use('jagi:astronomy-behaviors@0.5.2');
   api.use('underscore');
 
   // Behavior.
